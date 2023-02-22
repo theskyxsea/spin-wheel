ca1b4bcf7ee9d9ec54f215eaa260c62fa86bfc27
Akash K <akashksagar@gmail.com>
2023-02-22 12:58:54 +0000

    Simple spinning mechanism added

--- src/components/WheelComponent.tsx
+++ src/components/WheelComponent.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function WheelComponent() {
-  return <div></div>;
+import "./../styles/wheelComponent.css";
+
+function WheelComponent(props: any) {
+  const { items } = props;
+  const [selectedItem, setSelectedItem] = useState<any>("null");
+  const [wheelVars, setWheelVars] = useState<any>({
+    "--nb-item": items.length,
+  });
+  const [spinning, setSpinning] = useState<string>("");
+
+  const selectItem = async () => {
+    console.log("SelectItem");
+    console.log(selectedItem);
+    if (selectedItem === "null") {
+      await setSelectedItem(() => Math.floor(Math.random() * items.length));
+      console.log("Item Selected");
+      // this.setState({ selectedItem });
+    } else {
+      await setSelectedItem("null");
+      console.log()
+      await selectItem();
+      console.log("else");
+      // this.setState({ selectedItem: null });
+    }
+  };
+
+  useEffect(() => {
+    setSpinning(() => (selectedItem !== "null" ? "spinning" : ""));
+    //   // if (selectedItem === null) {
+    //   //   selectItem();
+    //   // }
+  }, [selectedItem]);
+
+  return (
+    <div className="wheel-container">
+      <div
+        className={`wheel ${spinning}`}
+        style={
+          {
+            ...wheelVars,
+            "--selected-item": selectedItem,
+          } as React.CSSProperties
+        }
+        onClick={() => selectItem()}
+      >
+        {items.map((item: any, index: any) => (
+          <div
+            className="wheel-item"
+            key={index}
+            style={{ "--item-nb": index } as React.CSSProperties}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default WheelComponent;
